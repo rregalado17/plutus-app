84fcc6c2db868f48c73ef3c2cefbc60a5a11752d
rregalado17 <rregalado17@gmail.com>
2022-11-10 16:52:59 -0600

    generate investor controller

---
+++ app/controllers/investor_controller.rb
@@ -0,0 +1,22 @@
+require_relative "auth_user"
+
+class InvestorController < ApplicationController
+    
+    include AuthUser
+    attr_accessor :first_name, :last_name, :username, :email, :password, :investments
+
+    def initialize(firstname, lastname, username, email, password)
+        @first_name = firstname  
+        @last_name = lastname
+        @username = username 
+        @email = email
+        @password = password
+    end
+end
+
+robinson = Investor.new("Robinson", "Regalado", "rregalado", "rregalado@email.com", "password1")
+
+hashed_password = robinson.create_hash_digest(robinson.password)
+p robinson
+puts hashed_password
+end
