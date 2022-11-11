041c69c262adea92d2d505895302339dee293a1c
rregalado17 <rregalado17@gmail.com>
2022-11-11 10:17:44 -0600

    add partials to forms

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,11 +1,53 @@
 class StocksController < ApplicationController
+    before_action :set_stock, only: [:show, :edit, :update, :destroy]
 
     def show 
-        @stock = Stock.find(params[:id])
     end
 
     def index
         @stocks = Stock.all
+    end
+
+    def new
+        @stock = Stock.new
+
+    end
+
+    def edit
+    end
+
+    def create
+        @stock = Stock.new(stock_params)
+        if @stock.save
+            flash[:notice] = "Stock created successfully!"
+            redirect_to @stock
+        else
+            render "new"
+        end
+    end
+
+    def update
+        if @stock.update(stock_params)
+            flash[:notice] = "Stock was updated successfully"
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @stock.destroy
+        redirect_to stocks_path
+
+    end
+
+    private 
+
+    def set_stock
+        @stock = Stock.find(params[:id])
+    end
+
+    def stock_params
+        params.require(:stock).permit(:name, :description)
     end
 
 
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,5 @@
 class Stock < ApplicationRecord
     validates :name, presence: true
+    validates :description, presence: true
 
 end
