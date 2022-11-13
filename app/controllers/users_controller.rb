682f256b94d5454b53537117b0500ab107bae1c3
rregalado17 <rregalado17@gmail.com>
2022-11-12 20:17:01 -0600

    update edit user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your profile has been successfully updated!"
+            redirect_to stocks_path
+        else
+            render 'edit'
+        end
+    end
+
     def create 
         @user = User.new(user_params)
         if @user.save
