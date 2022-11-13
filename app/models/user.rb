f7d841cd26f35942507a66aa57403e19d5c18798
rregalado17 <rregalado17@gmail.com>
2022-11-12 20:05:55 -0600

    add sign up features

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@
     validates :username, presence: true, uniqueness: { case_sensitive: false}, length: {minimum: 3, maximun: 25}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: { case_sensitive: false}, length: {minimum: 1, maximun: 105}, format: { with: VALID_EMAIL_REGEX }
-
+    has_secure_password
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'homepages#home'
   get 'about', to: 'homepages#about'
   resources :stocks
-  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
